0162c9539f7fd6d1244d42191b1334fc89ca7772
usernameAki <akisklep@gmail.com>
2023-09-17 14:36:41 +0200

    Add project files.

---
+++ AdventureScrolls/AdventureScrolls/Model/ScrollModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventureScrolls.Model
+{
+    public class ScrollModel
+    {
+        public DateTime EntryDate { get; set; }
+        public string Title { get; set; }
+        public string ScrollContent { get; set; }
+
+        public ScrollModel(string title, string scrollContext)
+        {
+            EntryDate = DateTime.Now;
+            Title = title;
+            ScrollContent = scrollContext;
+        }
+    }
+}
---
+++ AdventureScrolls/AdventureScrolls/ViewModel/AdventureScrollsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using AdventureScrolls.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace AdventureScrolls.ViewModel
+{
+    public class AdventureScrollsViewModel
+    {
+        public ObservableCollection<ScrollModel> Scrolls { get; set; }
+
+        public AdventureScrollsViewModel()
+        {
+            Scrolls = new ObservableCollection<ScrollModel>
+            {
+                new ScrollModel("tytuł 1", "mój wpis 1"),
+                new ScrollModel("tytuł 2", "mój wpis 2"),
+                new ScrollModel("tytuł 3", "mój wpis 3")
+            };
+        }
+    }
+}
