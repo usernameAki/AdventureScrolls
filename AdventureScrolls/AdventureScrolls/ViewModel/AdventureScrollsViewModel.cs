0331d454400517ef62134988c9b9dfd1d8990572
usernameAki <akisklep@gmail.com>
2023-09-26 20:53:37 +0200

    Aligned ListView entries in AdventureScrollsView.

--- AdventureScrolls/AdventureScrolls/ViewModel/AdventureScrollsViewModel.cs
+++ AdventureScrolls/AdventureScrolls/ViewModel/AdventureScrollsViewModel.cs
@@ -14,7 +14,7 @@ namespace AdventureScrolls.ViewModel
         {
             Scrolls = new ObservableCollection<ScrollModel>
             {
-                new ScrollModel("tytuł 1", "mój wpis 1"),
+                new ScrollModel("Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...\"", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
                 new ScrollModel("tytuł 2", "mój wpis 2"),
                 new ScrollModel("tytuł 3", "mój wpis 3"),
                 new ScrollModel("tytuł 4", "mój wpis 4"),
