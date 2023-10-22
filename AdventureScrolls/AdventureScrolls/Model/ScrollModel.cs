54037a9e0ce8b60991b51dae182f1808f5feef02
usernameAki <akisklep@gmail.com>
2023-10-22 14:48:08 +0200

    Fixed bug where date from date picker didn't passed value to new scroll.

--- AdventureScrolls/AdventureScrolls/Model/ScrollModel.cs
+++ AdventureScrolls/AdventureScrolls/Model/ScrollModel.cs
@@ -6,14 +6,14 @@ namespace AdventureScrolls.Model
 {
     public class ScrollModel
     {
-        public DateTimeOffset EntryDate { get; set; }
+        public DateTime EntryDate { get; set; }
         public string Title { get; set; }
         public string ScrollContent { get; set; }
         public string Mood { get; set; }
 
         public ScrollModel()
         {
-            EntryDate = DateTimeOffset.Now;
+            EntryDate = DateTime.Now;
             Title = "";
             ScrollContent = "";
             Mood = "happy";
