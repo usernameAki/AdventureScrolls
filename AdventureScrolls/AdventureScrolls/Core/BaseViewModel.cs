beeee160214f2d5ba04aa58991e13c1de8f9ec23
usernameAki <akisklep@gmail.com>
2023-11-10 19:37:27 +0100

    Added INavigationService to base class of ViewModels.

--- AdventureScrolls/AdventureScrolls/Core/BaseViewModel.cs
+++ AdventureScrolls/AdventureScrolls/Core/BaseViewModel.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Prism.Navigation;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AdventureScrolls.Core
 {
-    public class BaseViewModel : ObservableObjects
+    public abstract class BaseViewModel : ObservableObjects
     {
+        protected INavigationService NavigationService { get; }
+        public BaseViewModel(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+        }
     }
 }
