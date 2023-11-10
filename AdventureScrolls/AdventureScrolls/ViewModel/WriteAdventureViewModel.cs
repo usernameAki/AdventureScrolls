9a22fbbd4334a94949c413f6a4bcf2682b37034e
usernameAki <akisklep@gmail.com>
2023-11-10 19:51:53 +0100

    Added NavigationService. ScrollModel is now an observable object. Added editing mode. Adjusted navigation to Prism. Adjusted Commands to editing and creating.

--- AdventureScrolls/AdventureScrolls/ViewModel/WriteAdventureViewModel.cs
+++ AdventureScrolls/AdventureScrolls/ViewModel/WriteAdventureViewModel.cs
@@ -4,44 +4,88 @@ using AdventureScrolls.View;
 using AdventureScrolls.Model;
 using Rg.Plugins.Popup.Services;
 using AdventureScrolls.Services;
+using Prism.Navigation;
+using System.ComponentModel;
 
 namespace AdventureScrolls.ViewModel
 {
-    public class WriteAdventureViewModel : BaseViewModel
+    public class WriteAdventureViewModel : BaseViewModel, INavigationAware
     {
-        public ScrollModel Scroll { get; set; }
+        private bool _editingMode;
+        private ScrollModel _scroll;
+        public ScrollModel Scroll 
+        {
+            get => _scroll; 
+            set
+            {
+                _scroll = value;
+                OnPropertyChanged();
+            }
+        }
         public Command MoodButtonClicked { get; }
         public Command StoreScroll {  get; }
         public Command ChangeMood {  get; }
         private readonly IScrollCreatorService _scrollCreatorService;
         private readonly IScribeService _scribeService;
-        public WriteAdventureViewModel()
+        public WriteAdventureViewModel(INavigationService navigationService) : base(navigationService)
         {
             //Pass new scroll through DI
+            _editingMode = false;
             _scrollCreatorService = DependencyService.Get<IScrollCreatorService>();
             _scribeService = DependencyService.Get<IScribeService>();
-
             Scroll = _scrollCreatorService.NewScroll;
 
 
 
             //Commands
-            MoodButtonClicked = new Command(o => PopupNavigation.Instance.PushAsync(new MoodPopUpView()));
+
+            //Calls popup to change mood
+            MoodButtonClicked = new Command(o => 
+            {
+                var parameter = new NavigationParameters();
+                parameter.Add("Scroll", Scroll);
+                navigationService.NavigateAsync("MoodPopUpView", parameter);
+            });
+
+            //Stores new scroll
             StoreScroll = new Command(o => 
             { 
-                _scribeService.StoreNewScroll(Scroll);
-                Scroll.CleanData();
+                if(!_editingMode)
+                {
+                    _scribeService.StoreNewScroll(Scroll);
+                    Scroll.CleanData();
+                }
+                else
+                {
+                    _scribeService.OverrideScroll();
+                    _editingMode = false;
+                    Scroll = _scrollCreatorService.NewScroll;
+                    navigationService.NavigateAsync("/MainView");
+                }
             });
 
 
-
+            //Changes mood and closes popup
             ChangeMood = new Command(parameter =>
             {
-                _scrollCreatorService.NewScroll.Mood = parameter.ToString();
+                Scroll.Mood = parameter.ToString();
                 PopupNavigation.Instance.PopAsync();
             });
 
         }
 
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+            _editingMode = true;
+            Scroll = parameters.GetValue<ScrollModel>("Scroll");
+            //Scroll.ScrollContent = temp.ScrollContent;
+            //Scroll.Title = temp.Title;
+            //Scroll.EntryDate = temp.EntryDate;
+            //Scroll.Mood = temp.Mood;
+        }
     }
 }
